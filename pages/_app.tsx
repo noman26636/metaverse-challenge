746eb8d0bd377970e78cc41719ac5c7f12593a7e
noman26636 <manu26636@gmail.com>
2022-01-20 10:43:01 +0500

    moralis update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MoralisProvider } from 'react-moralis'
-import Moralis from 'moralis/types'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  const moralisAppID = process.env.NEXT_PUBLIC_APP_ID
-const moralisServerUrl = process.env.NEXT_PUBLIC_SERVER_URL
+   const app = process.env.NEXT_PUBLIC_APP_ID as string;
+   const url = process.env.NEXT_PUBLIC_SERVER_URL as string;
 
   return (
 
-    <MoralisProvider appId={moralisAppID} serverUrl={moralisServerUrl}>
+    <MoralisProvider appId={app} serverUrl={url}>
 
       <Component {...pageProps} />
     </MoralisProvider>
